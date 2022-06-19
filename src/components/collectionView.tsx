1318af147f328d056c9ca26d6bd847adacce07cd
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-06-19 17:42:49 +0200

    Creates first interfaces and classes

---
+++ src/components/collectionView.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Collection } from '../classes/collection.class';
+
+export class CollectionView extends React.Component<Collection> {
+    // constructor() {
+    //     super()
+    // }
+}
---
+++ src/interfaces/productStuff.interface.ts
@@ -0,0 +1,15 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    type: string;
+    image: string;
+    longDescription: string;
+    specs?: ISpecs;
+}
+export interface ISpecs {
+    actuationForce: string;
+    actuationPoint: string;
+    bottomOut: string;
+    bottomOutTravel: string;
+    price: string;
+}
